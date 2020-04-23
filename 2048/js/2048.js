755492783090d8347f19cf3c82d47343cb6193b3
juicymio <1612438624@qq.com>
2020-04-23 13:21:40 +0800

    finished

--- 2048/js/2048.js
+++ 2048/js/2048.js
@@ -1,5 +1,7 @@
 var game = 
 {
+    dy: [-1,1,0,0],
+    dx: [0,0,-1,1],
     data: [],
     score: 0,
     status: true,
@@ -7,7 +9,7 @@ var game =
     {
         this.status = true;
         this.score = 0;
-        game.data = 
+        this.data = 
         [
             [0,0,0,0],
             [0,0,0,0],
@@ -16,7 +18,6 @@ var game =
         ]
         this.generate();
         this.generate();
-        this.update();
     },
     generate: function()
     {
@@ -24,13 +25,14 @@ var game =
         {
             var r = Math.floor(Math.random()*4);
             var c = Math.floor(Math.random()*4);
-            var v = Math.random() > 0.5 ? 2 : 4;
+            var v = Math.random() > 0.1 ? 2 : 4;
             if (this.data[r][c] == 0)
             {
                 this.data[r][c] = v;
                 break;
             }
         }
+        this.update();
     },
     update: function()
     {
@@ -62,5 +64,207 @@ var game =
             document.getElementById("gameover").style.display = "none";
         }
     },
+    isGameover: function()
+    {
+        for (var r = 0;r < 4;r ++)
+            for (var c = 0;c < 4;c ++)
+                if (this.data[r][c] == 0) return false;
+        for (var r = 0;r < 3;r ++)
+            for (var c = 0;c < 3;c ++)
+                if (this.data[r][c] == this.data[r][c + 1] || this.data[r][c] == this.data[r + 1][c]) return false;
+        for (var i = 0;i < 3;i ++)
+            if (this.data[3][i] == this.data[3][i + 1] || this.data[i][3] == this.data[i + 1][3]) return false;
+        return true;
+    },
+    next: function(r, c, d)
+    {
+        var i = 0;
+        while (r < 4 && c < 4 && r > -1 && c > -1)
+        {
+            if (this.data[r][c] != 0) return i;
+            r += this.dx[d], c += this.dy[d];
+            i ++;
+        }
+        return -1;
+    },
+    moveLeft: function() 
+    {
+
+        var isMoved = false;
+        for (var r = 0;r < 4;r ++)
+        {
+            for (var c = 0;c < 3;c ++)
+            {
+                var delta = this.next(r, c + 1, 1);
+                var next_c = c + 1 + delta;
+                if (delta != -1)
+                {
+                    if (this.data[r][c] == 0)
+                    {
+                        this.data[r][c] = this.data[r][next_c];
+                        this.data[r][next_c] = 0;
+                        isMoved = true;
+                        c --; //还没有合并
+                    }
+                    else if (this.data[r][c] == this.data[r][next_c])
+                    {
+                        this.data[r][c] += this.data[r][next_c];
+                        this.data[r][next_c] = 0;
+                        this.score += this.data[r][c];
+                        isMoved = true;
+                    }
+                }
+                else 
+                {
+                    break;
+                }
+            }
+        }
+        if (isMoved)
+        {
+            this.generate();
+            if (this.isGameover()) this.status = false;
+        }
+        this.update();
+    },
+    moveRight: function() 
+    {
+
+        var isMoved = false;
+        for (var r = 0;r < 4;r ++)
+        {
+            for (var c = 3;c > 0;c --)
+            {
+                var delta = this.next(r, c - 1, 0);
+                var next_c = c - 1 - delta;
+                if (delta != -1)
+                {
+                    if (this.data[r][c] == 0)
+                    {
+                        this.data[r][c] = this.data[r][next_c];
+                        this.data[r][next_c] = 0;
+                        isMoved = true;
+                        c --; //还没有合并
+                    }
+                    else if (this.data[r][c] == this.data[r][next_c])
+                    {
+                        this.data[r][c] += this.data[r][next_c];
+                        this.data[r][next_c] = 0;
+                        this.score += this.data[r][c];
+                        isMoved = true;
+                    }
+                }
+                else 
+                {
+                    break;
+                }
+            }
+        }
+        if (isMoved)
+        {
+            this.generate();
+            if (this.isGameover()) this.status = false;
+        }
+        this.update();
+    },
+    moveUp: function()
+    {
+        var isMoved = false;
+        for (var c = 0;c < 4;c ++)
+        {
+            for (var r = 0;r < 3;r ++)
+            {
+                var delta = this.next(r + 1, c, 3);
+                var next_r = r + 1 + delta;
+                if (delta != -1)
+                {
+                    if (this.data[r][c] == 0)
+                    {
+                        this.data[r][c] = this.data[next_r][c];
+                        this.data[next_r][c] = 0;
+                        isMoved = true;
+                        c --; //还没有合并
+                    }
+                    else if (this.data[r][c] == this.data[next_r][c])
+                    {
+                        this.data[r][c] += this.data[next_r][c];
+                        this.data[next_r][c] = 0;
+                        this.score += this.data[r][c];
+                        isMoved = true;
+                    }
+                }
+                else 
+                {
+                    break;
+                }
+            }
+        }
+        if (isMoved)
+        {
+            this.generate();
+            if (this.isGameover()) this.status = false;
+        }
+        this.update();
+    },
+    moveDown: function()
+    {
+        var isMoved = false;
+        for (var c = 0;c < 4;c ++)
+        {
+            for (var r = 3;r > 0;r --)
+            {
+                var delta = this.next(r - 1, c, 2);
+                var next_r = r -1 - delta;
+                if (delta != -1)
+                {
+                    if (this.data[r][c] == 0)
+                    {
+                        this.data[r][c] = this.data[next_r][c];
+                        this.data[next_r][c] = 0;
+                        isMoved = true;
+                        c --; //还没有合并
+                    }
+                    else if (this.data[r][c] == this.data[next_r][c])
+                    {
+                        this.data[r][c] += this.data[next_r][c];
+                        this.data[next_r][c] = 0;
+                        this.score += this.data[r][c];
+                        isMoved = true;
+                    }
+                }
+                else 
+                {
+                    break;
+                }
+            }
+        }
+        if (isMoved)
+        {
+            this.generate();
+            if (this.isGameover()) this.status = false;
+        }
+        this.update();
+    }     
 }
 game.init();
+document.onkeydown=function(event)
+{
+    var e = event || window.event || arguments.callee.caller.arguments[0];
+    if (e && e.keyCode == 37)
+    {
+        game.moveLeft(); 
+    }
+    if (e && e.keyCode == 38)
+    {
+        game.moveUp();
+    }
+    if (e && e.keyCode == 39)
+    {
+        game.moveRight();
+    }
+    if (e && e.keyCode == 40)
+    {
+        game.moveDown();
+    }
+
+}; 
